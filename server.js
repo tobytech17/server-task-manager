627d9f8443cbe8fbe2938b5d32ed355b217c0427
tobytech17 <tobiojeleye17@gmail.com>
2026-03-13 05:41:40 -0700

    final push

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 const cors = require("cors")
 const mongoose = require("mongoose")
 const taskRoutes = require("./routes/taskRoutes")
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("./models/user");
 
 app.use(express.json())
 app.use(cors({
@@ -33,7 +35,88 @@ const startServer = async () => {
 }
 startServer()
 
-//error route
-app.use((req,res)=>{
-    res.status(401).json({success: false, message: "ROUTE NOT FOUND" })
-})
+//register api
+app.post("/api/register", async (req, res) => {
+  const { email, username, password } = req.body;
+
+  try {
+
+    const existingUser = await User.findOne({
+      $or: [{ email }, { username }]
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User with email or username already exists"
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      email,
+      username,
+      password: hashedPassword
+    });
+
+    await newUser.save();
+
+    res.json({
+      message: "User registered successfully"
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+});
+
+//login api
+app.post("/api/login", async (req, res) => {
+  const { identifier, password } = req.body;
+
+ console.log("Identifier received:", identifier);
+  
+ try {
+
+    const user = await User.findOne({
+      $or: [
+     { username: new RegExp(`^${identifier}$`, "i") },
+     { email: new RegExp(`^${identifier}$`, "i") }
+      ]
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found"
+      });
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        message: "Invalid password"
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      token,
+      username: user.username
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+});
